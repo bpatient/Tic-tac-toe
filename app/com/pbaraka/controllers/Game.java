5b230c2f0477a4b0809fa387f430ffc50dbce03d
baraka <webmaster@imali.biz>
2017-12-21 20:08:07 +0200

    version 1.2

--- app/com/pbaraka/controllers/Game.java
+++ app/com/pbaraka/controllers/Game.java
@@ -78,7 +78,19 @@ public class Game extends Controller {
 			else if(isDraw())
 				break;
 			else {
-				// Computer checks where to move in order to WIN or prevent HUMAN to win
+				// Computer checks where to move in order to WIN
+				for (int i = 0; i < SIDE_LENGTH; i++) {
+		            for (int j = 0; j < SIDE_LENGTH; j++) {
+		            	if(gameGrid[i][j] == COMPUTER) {
+		            		if(isComputerMoved(i, j)) {
+		            			// Break the next move to wait for input from HUMAN
+		    		            return;
+		            		}
+		            	}
+		            }		            
+		        }
+				
+				// Computer checks where to move in order to prevent HUMAN to win
 				for (int i = 0; i < SIDE_LENGTH; i++) {
 		            for (int j = 0; j < SIDE_LENGTH; j++) {
 		            	if(gameGrid[i][j] == EMPTY) {
@@ -158,79 +170,215 @@ public class Game extends Controller {
 	 * @return
 	 */
 	private static boolean isComputerMoved(int row, int col) {
-		// Check all cases on position [0,0] or [2,2]
-		if(row == col && (row == 0 || row == 2)) {
-			if (gameGrid[row][1] == gameGrid[row][2 - col] && gameGrid[row][1] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
+		/*
+		 *  Check all cases on position [0,0], [2,2], [0,2] or [2,0]
+		 */
+		if((row == col && (row == 0 || row == 2)) || (row == 0 && col == 2) || (row == 2 && row == 0)) {
+			/*
+			 *  Computer seeks to win
+			 */
+			if(gameGrid[row][col] == COMPUTER) {
+				// Row checking
+				if (gameGrid[row][col] == gameGrid[row][1] && gameGrid[row][2 - col] == EMPTY) {
+					gameGrid[row][2 - col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[row][2 - col] && gameGrid[row][1] == EMPTY) {
+					gameGrid[row][1] = COMPUTER;
+					return true;
+				}
+				
+				// Column checking
+				if (gameGrid[row][col] == gameGrid[1][col] && gameGrid[2 - row][col] == EMPTY) {
+					gameGrid[2 - row][col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[2 - row][col] && gameGrid[1][col] == EMPTY) {
+					gameGrid[1][col] = COMPUTER;
+					return true;
+				}
+				
+				// Diagonal checking
+				if (gameGrid[row][col] == gameGrid[1][1] && gameGrid[2 - row][2 - col] == EMPTY) {
+					gameGrid[2 - row][2 - col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[2 - row][2 - col] && gameGrid[1][1] == EMPTY) {
+					gameGrid[1][1] = COMPUTER;
+					return true;
+				}
 			}
-			if (gameGrid[1][col] == gameGrid[2 - row][col] && gameGrid[1][col] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
-			}
-			if (gameGrid[1][1] == gameGrid[2 - row][2 - col] && gameGrid[1][1] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
-			}
-	    }
-		
-		// Check all cases on position [0,2] or [2,0]
-		if((row == 0 && col == 2) || (row == 2 && row == 0)) {
-			if (gameGrid[row][1] == gameGrid[row][2 - col] && gameGrid[row][1] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
-			}
-			if (gameGrid[1][col] == gameGrid[2 - row][col] && gameGrid[1][col] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
-			}
-			if (gameGrid[1][1] == gameGrid[2 - row][2 - col] && gameGrid[1][1] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
+			else {
+				/*
+				 *  Computer prevents HUMAN to win
+				 */
+				if (gameGrid[row][1] == gameGrid[row][2 - col] && gameGrid[row][1]!= EMPTY) {
+					gameGrid[row][col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[1][col] == gameGrid[2 - row][col] && gameGrid[1][col]!= EMPTY) {
+					gameGrid[row][col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[1][1] == gameGrid[2 - row][2 - col] && gameGrid[1][1]!= EMPTY) {
+					gameGrid[row][col] = COMPUTER;
+					return true;
+				}
 			}
 	    }		
 		
-		// Check all cases on position [1,1]
+		/*
+		 *  Check all cases on position [1,1]
+		 */
 		if(row == col && row == 1) {
-			if (gameGrid[row][0] == gameGrid[row][2] && gameGrid[row][0] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
+			/*
+			 *  Computer seeks to win
+			 */
+			if(gameGrid[row][col] == COMPUTER) {
+				// Row checking
+				if (gameGrid[row][col] == gameGrid[row][0] && gameGrid[row][2] == EMPTY) {
+					gameGrid[row][2] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[row][2] && gameGrid[row][0] == EMPTY) {
+					gameGrid[row][0] = COMPUTER;
+					return true;
+				}
+				
+				// Column checking
+				if (gameGrid[row][col] == gameGrid[0][col] && gameGrid[2][col] == EMPTY) {
+					gameGrid[2][col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[2][col] && gameGrid[0][col] == EMPTY) {
+					gameGrid[0][col] = COMPUTER;
+					return true;
+				}
+				
+				// Diagonal checking
+				if (gameGrid[row][col] == gameGrid[0][0] && gameGrid[2][2] == EMPTY) {
+					gameGrid[2][2] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[2][2] && gameGrid[0][0] == EMPTY) {
+					gameGrid[0][0] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[0][2] && gameGrid[2][0] == EMPTY) {
+					gameGrid[2][0] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[2][0] && gameGrid[0][2] == EMPTY) {
+					gameGrid[0][2] = COMPUTER;
+					return true;
+				}
 			}
-			if (gameGrid[0][col] == gameGrid[2][col] && gameGrid[0][col] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
-			}
-			if (gameGrid[0][0] == gameGrid[2][2] && gameGrid[0][0] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
-			}
-			if (gameGrid[0][2] == gameGrid[2][0] && gameGrid[0][2] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
+			else {
+				/*
+				 *  Computer prevents HUMAN to win
+				 */
+				if (gameGrid[row][0] == gameGrid[row][2] && gameGrid[row][0] != EMPTY) {
+					gameGrid[row][col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[0][col] == gameGrid[2][col] && gameGrid[0][col] != EMPTY) {
+					gameGrid[row][col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[0][0] == gameGrid[2][2] && gameGrid[0][0] != EMPTY) {
+					gameGrid[row][col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[0][2] == gameGrid[2][0] && gameGrid[0][2] != EMPTY) {
+					gameGrid[row][col] = COMPUTER;
+					return true;
+				}
 			}
 	    }		
 		
-		// Check all cases on position [0,1], [2,1]
+		/*
+		 *  Check all cases on position [0,1], [2,1]
+		 */
 		if((row == 0 && col == 1) || (row == 2 && col == 1)) {
-			if (gameGrid[row][col - 1] == gameGrid[row][col + 1] && gameGrid[row][col - 1] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
+			/*
+			 *  Computer seeks to win
+			 */
+			if(gameGrid[row][col] == COMPUTER) {
+				// Row checking
+				if (gameGrid[row][col] == gameGrid[row][col - 1] && gameGrid[row][col + 1] == EMPTY) {
+					gameGrid[row][col + 1] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[row][col + 1] && gameGrid[row][col - 1] == EMPTY) {
+					gameGrid[row][col - 1] = COMPUTER;
+					return true;
+				}
+				
+				// Column checking
+				if (gameGrid[row][col] == gameGrid[1][1] && gameGrid[2 - row][col] == EMPTY) {
+					gameGrid[2 - row][col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[2 - row][col] && gameGrid[1][1] == EMPTY) {
+					gameGrid[1][1] = COMPUTER;
+					return true;
+				}
 			}
-			if (gameGrid[1][1] == gameGrid[2 - row][col] && gameGrid[1][1] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
+			else {
+				/*
+				 *  Computer prevents HUMAN to win
+				 */
+				if (gameGrid[row][col - 1] == gameGrid[row][col + 1] && gameGrid[row][col - 1] != EMPTY) {
+					gameGrid[row][col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[1][1] == gameGrid[2 - row][col] && gameGrid[1][1] != EMPTY) {
+					gameGrid[row][col] = COMPUTER;
+					return true;
+				}
 			}
 	    }
 		
-		// Check all cases on position [1,0], [1,2]
+		/*
+		 *  Check all cases on position [1,0], [1,2]
+		 */
 		if((row == 1 && col == 0) || (row == 1 && col == 2)) {
-			if (gameGrid[1][1] == gameGrid[row][2 - col] && gameGrid[1][1] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
+			/*
+			 *  Computer seeks to win
+			 */
+			if(gameGrid[row][col] == COMPUTER) {
+				// Row checking
+				if (gameGrid[row][col] == gameGrid[1][1] && gameGrid[row][2 - col] == EMPTY) {
+					gameGrid[row][2 - col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[row][2 - col] && gameGrid[1][1] == EMPTY) {
+					gameGrid[1][1] = COMPUTER;
+					return true;
+				}
+				
+				// Column checking
+				if (gameGrid[row][col] == gameGrid[row - 1][col] && gameGrid[row + 1][col] == EMPTY) {
+					gameGrid[row + 1][col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row][col] == gameGrid[row + 1][col] && gameGrid[row - 1][col] == EMPTY) {
+					gameGrid[row - 1][col] = COMPUTER;
+					return true;
+				}
 			}
-			if (gameGrid[row - 1][col] == gameGrid[row + 1][col] && gameGrid[row - 1][col] != EMPTY) {
-				gameGrid[row][col] = COMPUTER;
-				return true;
+			else {
+				/*
+				 *  Computer prevents HUMAN to win
+				 */
+				if (gameGrid[1][1] == gameGrid[row][2 - col] && gameGrid[1][1] != EMPTY) {
+					gameGrid[row][col] = COMPUTER;
+					return true;
+				}
+				if (gameGrid[row - 1][col] == gameGrid[row + 1][col] && gameGrid[row - 1][col] != EMPTY) {
+					gameGrid[row][col] = COMPUTER;
+					return true;
+				}
 			}
 	    }
 		
